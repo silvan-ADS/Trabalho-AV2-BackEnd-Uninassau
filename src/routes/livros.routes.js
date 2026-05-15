655e6bd0d14434d694ea8773e591c23373d67fe3
Lucas Alves <Lucasfael04@gmail.com>
2026-05-15 01:19:19 -0300

    correção

--- src/routes/livros.routes.js
+++ src/routes/livros.routes.js
@@ -3,11 +3,9 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  adicionarLivro,
-  listarLivros
+  adicionarLivro
 } = require("../controllers/livrosController");
 
-router.post("/", livrosController.adicionarLivro);
-router.get("/", livrosController.listarLivros);
+router.post("/", adicionarLivro);
 
 module.exports = router;

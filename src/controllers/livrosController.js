800561d5ec1844fa3745579c56622a0be135d2fd
Lucas Alves <Lucasfael04@gmail.com>
2026-05-15 06:46:54 -0300

    correção Listar Livros

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,15 +1,20 @@
 const livros = require("../models/livroModel");
 
+const listarLivros = (req, res) => {
+    res.json(livros)
+};
+
 const adicionarLivro = (req, res) => {
   const { titulo, autor, genero, anoPublicacao, quantidade, ISBN } = req.body;
 
   if (!titulo || !autor) {
     return res.status(400).json({
       erro: "Atenção: O título e o autor do livro são obrigatórios!",
-    });
-  }
+    })
+  };
 
   const novoLivro = {
+    id: livros.length + 1,
     titulo,
     autor,
     genero,
@@ -23,9 +28,10 @@ const adicionarLivro = (req, res) => {
   res.status(201).json({
     mensagem: "Livro adicionado com sucesso!",
     livroAdicionado: novoLivro,
-  });
+  })
 };
 
 module.exports = {
-  adicionarLivro
+  adicionarLivro,
+  listarLivros
 };

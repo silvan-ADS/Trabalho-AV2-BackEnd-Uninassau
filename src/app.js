7f302bea8d6c4f73490b81a32892e375bec87fa2
Lucas Alves <Lucasfael04@gmail.com>
2026-05-14 21:49:57 -0300

    Post Usuário

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 
+const usuariosRoutes = require("./routes/usuarios.routes");
+
 const app = express();
 
 app.use(express.json());
+
+app.use("/usuarios", usuariosRoutes);
 
 module.exports = app;
